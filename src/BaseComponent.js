58f42cc884b4f832396cacf3e4043babaa7650cc
Mike Wilcox <anm9tr@gmail.com>
2017-02-28 13:28:06 -0600

    add web components to npm dependency

--- src/BaseComponent.js
+++ src/BaseComponent.js
@@ -6,6 +6,7 @@
 
 // Classes http://exploringjs.com/es6/ch_classes.html#_the-species-pattern-in-static-methods
 
+require('./custom-elements-shim');
 const on = require('on');
 const dom = require('dom');
 
---
+++ src/custom-elements-shim.js
@@ -0,0 +1,2 @@
+require('@webcomponents/custom-elements/src/native-shim');
+require('@webcomponents/custom-elements/custom-elements.min');
