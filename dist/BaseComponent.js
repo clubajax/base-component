e6c331dd7ebb91fd96d2fd89e5f5ab99af85eae7
Mike Wilcox <anm9tr@gmail.com>
2017-03-21 07:26:17 -0500

    add polyfill

--- dist/BaseComponent.js
+++ dist/BaseComponent.js
@@ -1,261 +1,317 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD
-        define(["on", "dom"], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        // Node / CommonJS
-        module.exports = factory(require('on'), require('dom'));
-    } else {
-        // Browser globals (root is window)
-        root['BaseComponent'] = factory(root.on, root.dom);
-    }
-	}(this, function (on, dom) {
-'use strict';
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(["on", "dom"], factory);
+	}
+	else if (typeof module === 'object' && module.exports) {
+		// Node / CommonJS
+		module.exports = factory(require('on'), require('dom'));
+	}
+	else {
+		// Browser globals (root is window)
+		root['BaseComponent'] = factory(root.on, root.dom);
+	}
+}(this, function (on, dom) {
+	'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	var _createClass = function () {
+		function defineProperties(target, props) {
+			for (var i = 0; i < props.length; i++) {
+				var descriptor = props[i];
+				descriptor.enumerable = descriptor.enumerable || false;
+				descriptor.configurable = true;
+				if ("value" in descriptor) {
+					descriptor.writable = true;
+				}
+				Object.defineProperty(target, descriptor.key, descriptor);
+			}
+		}
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+		return function (Constructor, protoProps, staticProps) {
+			if (protoProps) {
+				defineProperties(Constructor.prototype, protoProps);
+			}
+			if (staticProps) {
+				defineProperties(Constructor, staticProps);
+			}
+			return Constructor;
+		};
+	}();
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	function _classCallCheck(instance, Constructor) {
+		if (!(instance instanceof Constructor)) {
+			throw new TypeError("Cannot call a class as a function");
+		}
+	}
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	function _possibleConstructorReturn(self, call) {
+		if (!self) {
+			throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+		}
+		return call && (typeof call === "object" || typeof call === "function") ? call : self;
+	}
 
-var BaseComponent = function (_HTMLElement) {
-    _inherits(BaseComponent, _HTMLElement);
+	function _inherits(subClass, superClass) {
+		if (typeof superClass !== "function" && superClass !== null) {
+			throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+		}
+		subClass.prototype = Object.create(superClass && superClass.prototype, {
+			constructor: {
+				value: subClass,
+				enumerable: false,
+				writable: true,
+				configurable: true
+			}
+		});
+		if (superClass) {
+			Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+		}
+	}
 
-    function BaseComponent() {
-        _classCallCheck(this, BaseComponent);
+	var BaseComponent = function (_HTMLElement) {
+		_inherits(BaseComponent, _HTMLElement);
 
-        var _this = _possibleConstructorReturn(this, (BaseComponent.__proto__ || Object.getPrototypeOf(BaseComponent)).call(this));
+		function BaseComponent() {
+			_classCallCheck(this, BaseComponent);
 
-        _this._uid = dom.uid(_this.localName);
-        privates[_this._uid] = { DOMSTATE: 'created' };
-        privates[_this._uid].handleList = [];
-        plugin('init', _this);
-        return _this;
-    }
+			var _this = _possibleConstructorReturn(this, (BaseComponent.__proto__ || Object.getPrototypeOf(BaseComponent)).call(this));
 
-    _createClass(BaseComponent, [{
-        key: 'connectedCallback',
-        value: function connectedCallback() {
-            privates[this._uid].DOMSTATE = 'connected';
-            plugin('preConnected', this);
-            nextTick(onCheckDomReady.bind(this));
-            if (this.connected) {
-                this.connected();
-            }
-            this.fire('connected');
-            plugin('postConnected', this);
-        }
-    }, {
-        key: 'disconnectedCallback',
-        value: function disconnectedCallback() {
-            privates[this._uid].DOMSTATE = 'disconnected';
-            plugin('preDisconnected', this);
-            if (this.disconnected) {
-                this.disconnected();
-            }
-            this.fire('disconnected');
-        }
-    }, {
-        key: 'attributeChangedCallback',
-        value: function attributeChangedCallback(attrName, oldVal, newVal) {
-            plugin('preAttributeChanged', this, attrName, newVal, oldVal);
-            if (this.attributeChanged) {
-                this.attributeChanged(attrName, newVal, oldVal);
-            }
-        }
-    }, {
-        key: 'destroy',
-        value: function destroy() {
-            this.fire('destroy');
-            privates[this._uid].handleList.forEach(function (handle) {
-                handle.remove();
-            });
-            dom.destroy(this);
-        }
-    }, {
-        key: 'fire',
-        value: function fire(eventName, eventDetail, bubbles) {
-            return on.fire(this, eventName, eventDetail, bubbles);
-        }
-    }, {
-        key: 'emit',
-        value: function emit(eventName, value) {
-            return on.emit(this, eventName, value);
-        }
-    }, {
-        key: 'on',
-        value: function (_on) {
-            function on(_x, _x2, _x3, _x4) {
-                return _on.apply(this, arguments);
-            }
+			_this._uid = dom.uid(_this.localName);
+			privates[_this._uid] = {DOMSTATE: 'created'};
+			privates[_this._uid].handleList = [];
+			plugin('init', _this);
+			return _this;
+		}
 
-            on.toString = function () {
-                return _on.toString();
-            };
+		_createClass(BaseComponent, [{
+			key: 'connectedCallback',
+			value: function connectedCallback() {
+				privates[this._uid].DOMSTATE = 'connected';
+				plugin('preConnected', this);
+				nextTick(onCheckDomReady.bind(this));
+				if (this.connected) {
+					this.connected();
+				}
+				this.fire('connected');
+				plugin('postConnected', this);
+			}
+		}, {
+			key: 'disconnectedCallback',
+			value: function disconnectedCallback() {
+				privates[this._uid].DOMSTATE = 'disconnected';
+				plugin('preDisconnected', this);
+				if (this.disconnected) {
+					this.disconnected();
+				}
+				this.fire('disconnected');
+			}
+		}, {
+			key: 'attributeChangedCallback',
+			value: function attributeChangedCallback(attrName, oldVal, newVal) {
+				plugin('preAttributeChanged', this, attrName, newVal, oldVal);
+				if (this.attributeChanged) {
+					this.attributeChanged(attrName, newVal, oldVal);
+				}
+			}
+		}, {
+			key: 'destroy',
+			value: function destroy() {
+				this.fire('destroy');
+				privates[this._uid].handleList.forEach(function (handle) {
+					handle.remove();
+				});
+				dom.destroy(this);
+			}
+		}, {
+			key: 'fire',
+			value: function fire(eventName, eventDetail, bubbles) {
+				return on.fire(this, eventName, eventDetail, bubbles);
+			}
+		}, {
+			key: 'emit',
+			value: function emit(eventName, value) {
+				return on.emit(this, eventName, value);
+			}
+		}, {
+			key: 'on',
+			value: function (_on) {
+				function on(_x, _x2, _x3, _x4) {
+					return _on.apply(this, arguments);
+				}
 
-            return on;
-        }(function (node, eventName, selector, callback) {
-            return this.registerHandle(typeof node != 'string' ? // no node is supplied
-            on(node, eventName, selector, callback) : on(this, node, eventName, selector));
-        })
-    }, {
-        key: 'once',
-        value: function once(node, eventName, selector, callback) {
-            return this.registerHandle(typeof node != 'string' ? // no node is supplied
-            on.once(node, eventName, selector, callback) : on.once(this, node, eventName, selector, callback));
-        }
-    }, {
-        key: 'registerHandle',
-        value: function registerHandle(handle) {
-            privates[this._uid].handleList.push(handle);
-            return handle;
-        }
-    }, {
-        key: 'DOMSTATE',
-        get: function get() {
-            return privates[this._uid].DOMSTATE;
-        }
-    }], [{
-        key: 'clone',
-        value: function clone(template) {
-            if (template.content && template.content.children) {
-                return document.importNode(template.content, true);
-            }
-            var frag = document.createDocumentFragment(),
-                cloneNode = document.createElement('div');
-            cloneNode.innerHTML = template.innerHTML;
+				on.toString = function () {
+					return _on.toString();
+				};
 
-            while (cloneNode.children.length) {
-                frag.appendChild(cloneNode.children[0]);
-            }
-            return frag;
-        }
-    }, {
-        key: 'addPlugin',
-        value: function addPlugin(plug) {
-            var i = void 0,
-                order = plug.order || 100;
-            if (!plugins.length) {
-                plugins.push(plug);
-            } else if (plugins.length === 1) {
-                if (plugins[0].order <= order) {
-                    plugins.push(plug);
-                } else {
-                    plugins.unshift(plug);
-                }
-            } else if (plugins[0].order > order) {
-                plugins.unshift(plug);
-            } else {
+				return on;
+			}(function (node, eventName, selector, callback) {
+				return this.registerHandle(typeof node != 'string' ? // no node is supplied
+					on(node, eventName, selector, callback) : on(this, node, eventName, selector));
+			})
+		}, {
+			key: 'once',
+			value: function once(node, eventName, selector, callback) {
+				return this.registerHandle(typeof node != 'string' ? // no node is supplied
+					on.once(node, eventName, selector, callback) : on.once(this, node, eventName, selector, callback));
+			}
+		}, {
+			key: 'registerHandle',
+			value: function registerHandle(handle) {
+				privates[this._uid].handleList.push(handle);
+				return handle;
+			}
+		}, {
+			key: 'DOMSTATE',
+			get: function get() {
+				return privates[this._uid].DOMSTATE;
+			}
+		}], [{
+			key: 'clone',
+			value: function clone(template) {
+				if (template.content && template.content.children) {
+					return document.importNode(template.content, true);
+				}
+				var frag = document.createDocumentFragment(),
+					cloneNode = document.createElement('div');
+				cloneNode.innerHTML = template.innerHTML;
 
-                for (i = 1; i < plugins.length; i++) {
-                    if (order === plugins[i - 1].order || order > plugins[i - 1].order && order < plugins[i].order) {
-                        plugins.splice(i, 0, plug);
-                        return;
-                    }
-                }
-                // was not inserted...
-                plugins.push(plug);
-            }
-        }
-    }]);
+				while (cloneNode.children.length) {
+					frag.appendChild(cloneNode.children[0]);
+				}
+				return frag;
+			}
+		}, {
+			key: 'addPlugin',
+			value: function addPlugin(plug) {
+				var i = void 0,
+					order = plug.order || 100;
+				if (!plugins.length) {
+					plugins.push(plug);
+				}
+				else if (plugins.length === 1) {
+					if (plugins[0].order <= order) {
+						plugins.push(plug);
+					}
+					else {
+						plugins.unshift(plug);
+					}
+				}
+				else if (plugins[0].order > order) {
+					plugins.unshift(plug);
+				}
+				else {
 
-    return BaseComponent;
-}(HTMLElement);
+					for (i = 1; i < plugins.length; i++) {
+						if (order === plugins[i - 1].order || order > plugins[i - 1].order && order < plugins[i].order) {
+							plugins.splice(i, 0, plug);
+							return;
+						}
+					}
+					// was not inserted...
+					plugins.push(plug);
+				}
+			}
+		}]);
 
-var privates = {},
-    plugins = [];
+		return BaseComponent;
+	}(HTMLElement);
 
-function plugin(method, node, a, b, c) {
-    plugins.forEach(function (plug) {
-        if (plug[method]) {
-            plug[method](node, a, b, c);
-        }
-    });
-}
+	var privates = {},
+		plugins = [];
 
-function onCheckDomReady() {
-    if (this.DOMSTATE != 'connected' || privates[this._uid].domReadyFired) {
-        return;
-    }
+	function plugin(method, node, a, b, c) {
+		plugins.forEach(function (plug) {
+			if (plug[method]) {
+				plug[method](node, a, b, c);
+			}
+		});
+	}
 
-    var count = 0,
-        children = getChildCustomNodes(this),
-        ourDomReady = onDomReady.bind(this);
+	function onCheckDomReady() {
+		if (this.DOMSTATE != 'connected' || privates[this._uid].domReadyFired) {
+			return;
+		}
 
-    function addReady() {
-        count++;
-        if (count == children.length) {
-            ourDomReady();
-        }
-    }
+		var count = 0,
+			children = getChildCustomNodes(this),
+			ourDomReady = onDomReady.bind(this);
 
-    // If no children, we're good - leaf node. Commence with onDomReady
-    //
-    if (!children.length) {
-        ourDomReady();
-    } else {
-        // else, wait for all children to fire their `ready` events
-        //
-        children.forEach(function (child) {
-            // check if child is already ready
-            if (child.DOMSTATE == 'domready') {
-                addReady();
-            }
-            // if not, wait for event
-            child.on('domready', addReady);
-        });
-    }
-}
+		function addReady() {
+			count++;
+			if (count == children.length) {
+				ourDomReady();
+			}
+		}
 
-function onDomReady() {
-    privates[this._uid].DOMSTATE = 'domready';
-    // domReady should only ever fire once
-    privates[this._uid].domReadyFired = true;
-    plugin('preDomReady', this);
-    // call this.domReady first, so that the component
-    // can finish initializing before firing any
-    // subsequent events
-    if (this.domReady) {
-        this.domReady();
-        this.domReady = function () {};
-    }
+		// If no children, we're good - leaf node. Commence with onDomReady
+		//
+		if (!children.length) {
+			ourDomReady();
+		}
+		else {
+			// else, wait for all children to fire their `ready` events
+			//
+			children.forEach(function (child) {
+				// check if child is already ready
+				if (child.DOMSTATE == 'domready') {
+					addReady();
+				}
+				// if not, wait for event
+				child.on('domready', addReady);
+			});
+		}
+	}
 
-    this.fire('domready');
+	function onDomReady() {
+		privates[this._uid].DOMSTATE = 'domready';
+		// domReady should only ever fire once
+		privates[this._uid].domReadyFired = true;
+		plugin('preDomReady', this);
+		// call this.domReady first, so that the component
+		// can finish initializing before firing any
+		// subsequent events
+		if (this.domReady) {
+			this.domReady();
+			this.domReady = function () {
+			};
+		}
 
-    plugin('postDomReady', this);
-}
+		this.fire('domready');
 
-function getChildCustomNodes(node) {
-    // collect any children that are custom nodes
-    // used to check if their dom is ready before
-    // determining if this is ready
-    var i = void 0,
-        nodes = [];
-    for (i = 0; i < node.children.length; i++) {
-        if (node.children[i].nodeName.indexOf('-') > -1) {
-            nodes.push(node.children[i]);
-        }
-    }
-    return nodes;
-}
+		plugin('postDomReady', this);
+	}
 
-function nextTick(cb) {
-    requestAnimationFrame(cb);
-}
+	function getChildCustomNodes(node) {
+		// collect any children that are custom nodes
+		// used to check if their dom is ready before
+		// determining if this is ready
+		var i = void 0,
+			nodes = [];
+		for (i = 0; i < node.children.length; i++) {
+			if (node.children[i].nodeName.indexOf('-') > -1) {
+				nodes.push(node.children[i]);
+			}
+		}
+		return nodes;
+	}
 
-window.onDomReady = function (node, callback) {
-    function onReady() {
-        callback(node);
-        node.removeEventListener('domready', onReady);
-    }
-    if (node.DOMSTATE === 'domready') {
-        callback(node);
-    } else {
-        node.addEventListener('domready', onReady);
-    }
-};
+	function nextTick(cb) {
+		requestAnimationFrame(cb);
+	}
+
+	window.onDomReady = function (node, callback) {
+		function onReady() {
+			callback(node);
+			node.removeEventListener('domready', onReady);
+		}
+
+		if (node.DOMSTATE === 'domready') {
+			callback(node);
+		}
+		else {
+			node.addEventListener('domready', onReady);
+		}
+	};
 
 	return BaseComponent;
 
